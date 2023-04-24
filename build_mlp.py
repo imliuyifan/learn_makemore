cac44e709ed9c1da9faae5c2ab344c7e17fdcca9
Yifan Liu <imliuyifan@gmail.com>
2023-04-24 09:51:21 -0400

    starting point for makemore Part 3

--- build_mlp.py
+++ build_mlp.py
@@ -6,48 +6,35 @@ import matplotlib.pyplot as plt # for making plots
 words = open('names.txt').read().splitlines()
 words[:8]
 
+print(len(words))
+
 # build the vocabulary of characters and mapping to/from integers
 chars = sorted(list(set(''.join(words))))
 stoi = {s:i+1 for i,s in enumerate(chars)}
 stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
+vocab_size = len(itos)
 print(itos)
-
-# # build the dataset
-# block_size = 3
-# X, Y = [], []
-# for w in words:
-#     # print(w)
-#     context = [0] * block_size
-#     for ch in w + '.':
-#         ix = stoi[ch]
-#         X.append(context[:])
-#         Y.append(ix)
-#         # print(''.join([itos[i] for i in context]), '--->', itos[ix])
-#         context = context[1:] + [ix]
-
-# # convert to tensors
-# X = torch.tensor(X, dtype=torch.long)
-# Y = torch.tensor(Y, dtype=torch.long)
-# print(X.shape, Y.shape)
+print(vocab_size)
 
 # build the dataset
 
+block_size = 3 # context length: how many characters do we take to predict the next one?
+
 def build_dataset(words):
-    block_size = 3
     X, Y = [], []
     for w in words:
-        # print(w)
         context = [0] * block_size
         for ch in w + '.':
             ix = stoi[ch]
             X.append(context[:])
             Y.append(ix)
             # print(''.join([itos[i] for i in context]), '--->', itos[ix])
-            context = context[1:] + [ix]
+            context = context[1:] + [ix] # crop and append
     # convert to tensors
     X = torch.tensor(X, dtype=torch.long)
     Y = torch.tensor(Y, dtype=torch.long)
+    print(X.shape, Y.shape)
     return X, Y
 
 import random
@@ -55,17 +42,24 @@ random.seed(42)
 random.shuffle(words)
 n1 = int(len(words) * 0.8)
 n2 = int(len(words) * 0.9)
-Xtr, Ytr = build_dataset(words[:n1])
-Xdev, Ydev = build_dataset(words[n1:n2])
-Xte, Yte = build_dataset(words[n2:])
 
-g = torch.Generator().manual_seed(2147483647)
-C = torch.randn((27, 10), generator=g)
+Xtr, Ytr = build_dataset(words[:n1])      # 80%
+Xdev, Ydev = build_dataset(words[n1:n2])  # 10%
+Xte, Yte = build_dataset(words[n2:])      # 10%
+
+# MLP revisited
+n_embd = 10 # the dimensionality of the character embedding vector
+n_hidden = 200 # the number of neurons in the hidden layer of the MLP
+
+g = torch.Generator().manual_seed(2147483647) # for reproducibility
+C = torch.randn((vocab_size, n_embd),             generator=g)
+
 # construct the hidden layer
-W1 = torch.randn((30, 200), generator=g)
-b1 = torch.randn(200, generator=g)
-W2 = torch.randn((200, 27), generator=g)
-b2 = torch.randn(27, generator=g)
+W1 = torch.randn((n_embd * block_size, n_hidden), generator=g)
+b1 = torch.randn(n_hidden,                        generator=g)
+W2 = torch.randn((n_hidden, vocab_size),          generator=g)
+b2 = torch.randn(vocab_size,                      generator=g)
+
 parameters = [C, W1, b1, W2, b2]
 num_param = sum(p.nelement() for p in parameters)
 print(f'number of parameters: {num_param}')
@@ -74,70 +68,74 @@ print(f'number of parameters: {num_param}')
 for p in parameters:
     p.requires_grad = True
 
-# try different learning rates
-lre = torch.linspace(-3, 0, 1000)
-lrs = 10 ** lre
 
-lri = []
+# same optimization as last time
+max_steps = 200000
+batch_size = 32
 lossi = []
-stepi = []
 
-for i in range(200000):
+for i in range(max_steps):
     # mini-batch construct
-    ix = torch.randint(0, Xtr.shape[0], (32,))
+    ix = torch.randint(0, Xtr.shape[0], (batch_size,), generator=g)
+    Xb, Yb = Xtr[ix], Ytr[ix] # batch X,Y
     # forward pass
-    emb = C[Xtr[ix]]
-    h = torch.tanh(emb.view(-1, 30) @ W1 + b1)
-    logits = h @ W2 + b2
-    loss = F.cross_entropy(logits, Ytr[ix])
+    emb = C[Xb]
+    embcat = emb.view(emb.shape[0], -1) # concatenate the vectors
+    hpreact = embcat @ W1 + b1 # hidden layer pre-activation
+    h = torch.tanh(hpreact) # hidden layer 
+    logits = h @ W2 + b2 # output layer
+    loss = F.cross_entropy(logits, Ytr[ix]) # loss function
     # print(loss.item())
     # backward pass
     for p in parameters:
         p.grad = None
     loss.backward()
     # update parameters
-    # lr = lrs[i]
     lr = 0.1 if i < 10000 else 0.01
     for p in parameters:
         p.data += -lr * p.grad
-    # track loss
-    # lri.append(lre[i].item())
-    stepi.append(i)
+    # track stats
+    if i % 10000 == 0: # print every 10000 steps
+        print(f'{i:7d}/{max_steps:7d}: {loss.item():.4f}')
     lossi.append(loss.log10().item())
 
-print(loss.item())
 
-emb = C[Xdev]
-h = torch.tanh(emb.view(-1, 30) @ W1 + b1)
-logits = h @ W2 + b2
-loss = F.cross_entropy(logits, Ydev)
-print(loss.item())
+@torch.no_grad() # this decorator disables gradient computation
+def split_loss(split):
+    x,y = {
+            'train': (Xtr, Ytr),
+            'val': (Xdev, Ydev),
+            'test': (Xte, Yte),
+            }[split]
+    emb = C[x] # (N, block_size, n_embd)
+    embcat = emb.view(emb.shape[0], -1) # concatenate into (N, block_size * n_embd)
+    hpreact = embcat @ W1 + b1 # hidden layer pre-activation
+    h = torch.tanh(hpreact) # hidden layer
+    logits = h @ W2 + b2 # output layer
+    loss = F.cross_entropy(logits, y)
+    print(f'{split} loss: {loss.item():.4f}')
 
-# training split, dev/validation split, test split
-# 80%, 10%, 10%
+split_loss('train')
+split_loss('val')
 
-# visualize the embedding
-# plt.figure(figsize=(10, 10))
-# plt.scatter(C[:, 0].data, C[:, 1].data, s=200)
-# for i in range(C.shape[0]):
-#     plt.text(C[i, 0].item(), C[i, 1].item(), itos[i], ha='center', va='center', color='white')
-# plt.grid('minor')
 
 # sample from the model
 g = torch.Generator().manual_seed(2147483647 + 10)
 block_size = 3
 
-for _ in range(50):
+for _ in range(20):
     out = []
     context = [0] * block_size # initialize the all ...
     while True:
-        emb = C[torch.tensor([context])]
+        emb = C[torch.tensor([context])] # (1, block_size, n_embd)
         h = torch.tanh(emb.view(1, -1) @ W1 + b1)
         logits = h @ W2 + b2
         probs = F.softmax(logits, dim=1)
+        # sample from tthe distribution
         ix = torch.multinomial(probs, num_samples=1, generator=g).item()
         context = context[1:] + [ix]
         out.append(ix)
+        # if we sample the special '.' token, break
         if ix == 0:
             break
-    print(''.join([itos[i] for i in out]))
+    print(''.join([itos[i] for i in out])) # decode and print the generated word
